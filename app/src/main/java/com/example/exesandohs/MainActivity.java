f5da49296c670826ca4e165d611813dbd0286b80
timothyfox443 <timothyfox443@gmail.com>
2021-10-27 12:12:39 -0700

    created varaibles to contain references to btns started with the onClick parameters in design view but not sure how to use them began with the btnClicked method but still need some work done to it

--- app/src/main/java/com/example/exesandohs/MainActivity.java
+++ app/src/main/java/com/example/exesandohs/MainActivity.java
@@ -3,14 +3,44 @@ package com.example.exesandohs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity
 {
+    int[][] btnsClicked = new int[9][2];
+    private Button btnOne;
+    private Button btnTwo;
+    private Button btnThree;
+    private Button btnFour;
+    private Button btnFive;
+    private Button btnSix;
+    private Button btnSeven;
+    private Button btnEight;
+    private Button btnNine;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOne = findViewById(R.id.btnOne);
+        btnTwo = findViewById(R.id.btnTwo);
+        btnThree = findViewById(R.id.btnThree);
+        btnFour = findViewById(R.id.btnFour);
+        btnFive = findViewById(R.id.btnFive);
+        btnSix = findViewById(R.id.btnSix);
+        btnSeven = findViewById(R.id.btnSeven);
+        btnEight = findViewById(R.id.btnEight);
+        btnNine = findViewById(R.id.btnNine);
     }
+
+    public void btnClicked(int btnClickIn)
+    {
+        if (btnClickIn )
+    }
+
 }
